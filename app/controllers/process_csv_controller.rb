70a93fe0233025c98a4e4e8efe850c54baad118c
Cristian <cristian1853@gmail.com>
2015-06-08 23:54:37 -0500

    first commit

---
+++ app/controllers/process_csv_controller.rb
@@ -0,0 +1,43 @@
+class ProcessCsvController < ApplicationController
+  require 'csv'
+  require 'ZipFileGenerator.rb'
+  def index
+  end
+
+  def import
+	file = params[:file]
+	dZip = "tmp/csvExports"
+	dFZip = dZip + ".zip"
+	FileUtils.rm_rf(dZip)
+	FileUtils.mkdir_p(dZip)
+	#@filecsv = SmarterCSV.process(file.path).uniq! { |a| a.first }
+	puts file.path
+	@filecsv = CSV.read(file.path).uniq! { |a| a.first }
+	@filecsv = @filecsv.uniq! { |a| a[3]}
+	header = @filecsv.first
+	cant_rows_per_file = 250;
+	numFiles = @filecsv.size/cant_rows_per_file
+	
+	for i in 0..numFiles
+		nameFile = dZip.to_s + "/clear" + i.to_s + ".csv"
+		CSV.open(nameFile,"wb") do |csv|
+			start_row = cant_rows_per_file*i+1
+			end_row = cant_rows_per_file*(i+1)
+			csv << header
+			if i < numFiles
+				for j in start_row..end_row
+					csv << @filecsv[j]
+				end
+			elsif
+				for j in start_row..@filecsv.size-1
+					csv << @filecsv[j]
+				end
+			end
+		end
+	end
+	zf = ZipFileGenerator.new(dZip,dFZip)
+	zf.write()
+	send_file dFZip
+	FileUtils.rm_rf(dZip)
+  end
+end
---
+++ app/helpers/process_csv_helper.rb
@@ -0,0 +1,2 @@
+module ProcessCsvHelper
+end
